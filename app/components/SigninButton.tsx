d4a4f26cba54e5f2f4f560044e4a2a2ecc84347f
boon1137 <booncomart@gmail.com>
2023-06-24 19:06:53 +0700

    final

---
+++ app/components/SigninButton.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+const SigninButton = () => {
+  const { data: session } = useSession();
+
+  if (session && session.user) {
+    return (
+      <div className="flex gap-4 ml-auto items-center">
+        <Image
+              src={session.user.image}
+              width={40}
+              height={40}
+              className="rounded-full"
+            />
+        <p className="text-sky-600 ">{session.user.name}</p>
+        <button onClick={() => signOut()} className="text-red-600">
+          Sign Out
+        </button>
+      </div>
+    );
+  }
+  return (
+    <button onClick={() => signIn()} className="text-green-600 ml-auto">
+      Sign In
+    </button>
+  );
+};
+
+export default SigninButton;
